99a29caef2ed8233b61ff601c9dde134ded47cd2
natal <yohansod@gmail.com>
2024-08-13 23:17:54 +0200

    Update npm packages to latest versions, correct routes/capmgrounds.js according to latest features of mongoose
    Remove node_modules from git tracking
    
    Correct comments.js and index.js due to new features of mongoose

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,52 +5,40 @@ var express     = require("express"),
 
 
 //INDEX - show all campgrounds
-router.get("/", function(req, res){
-    // Fuzzy Search
-    if (req.query.search) {
-        var regex = new RegExp(escapeRegex(req.query.search), 'gi');
-        // Get all campgrounds that we are searching
-        Campground.find({name: regex}, function(err, allCampgrounds){
-            if(err){
-                console.log(err);
-            } else {
-                res.render("campgrounds/index",{campgrounds: allCampgrounds});
-            }
-        });
-    } else {
-    // Get all campgrounds from DB
-        Campground.find({}, function(err, allCampgrounds){
-        if(err){
-            console.log(err);
+router.get("/", async (req, res) => {
+    try {
+        let allCampgrounds;
+        if (req.query.search) {
+            const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+            allCampgrounds = await Campground.find({name: regex});
         } else {
-            res.render("campgrounds/index",{campgrounds: allCampgrounds});
+            allCampgrounds = await Campground.find({});
         }
-        });
+        res.render("campgrounds/index", {campgrounds: allCampgrounds});
+    } catch (err) {
+        console.error(err);
+        req.flash("error", "An error occurred while fetching campgrounds");
+        res.redirect("/");
     }
 });
 
 //CREATE - add a new campground to DB
-router.post("/", middleware.isLoggedIn, function(req, res){
-    //get data from form and add to campgrounds array 
-    var name = req.body.name;
-    var price = req.body.price;
-    var image = req.body.image;
-    var desc = req.body.description;
-    var author = {
-        id: req.user._id,
-        username: req.user.username
+router.post("/", middleware.isLoggedIn, async (req, res) => {
+    try {
+        const { name, price, image, description } = req.body;
+        const author = {
+            id: req.user._id,
+            username: req.user.username
+        };
+        const newCampground = { name, price, image, description, author };
+        await Campground.create(newCampground);
+        req.flash("success", "Campground created successfully");
+        res.redirect("/campgrounds");
+    } catch (err) {
+        console.error(err);
+        req.flash("error", "Error creating campground");
+        res.redirect("/campgrounds");
     }
-    var newCampground = {name: name, price: price, image: image, description: desc, author: author}
-    //Create new campground and save it to DB
-    Campground.create(newCampground, function(err, newlyCreated){
-        if(err){
-            console.log(err);
-        } else {
-            //redirect back to campgrounds page
-            res.redirect("/campgrounds");
-        }
-    });
-    
 });
 
 //NEW - show from to add a new campground
@@ -59,102 +47,74 @@ router.get("/new", middleware.isLoggedIn, function(req, res){
 });
 
 //SHOW - shows more info about one campground
-router.get("/:id", function (req, res){
-    //find campfround with provided ID
-    Campground.findById(req.params.id).populate("comments likes").exec(function(err, foundCampground){
-        if(err){
-        console.log(err)
-        } else {
-            console.log(foundCampground);
-            //render show tamplate with that campground
-            res.render("campgrounds/show", {campground: foundCampground});
-        }
-    });
+router.get("/:id", async (req, res) => {
+    try {
+        const foundCampground = await Campground.findById(req.params.id).populate("comments likes");
+        res.render("campgrounds/show", {campground: foundCampground});
+    } catch (err) {
+        console.error(err);
+        req.flash("error", "Campground not found");
+        res.redirect("/campgrounds");
+    }
 });
 
 // EDIT CAMPGROUND ROUTE
-router.get("/:id/edit", middleware.checkCampgroundOwnership ,function(req,  res){
-    Campground.findById(req.params.id, function(err, foundCampground){
+router.get("/:id/edit", middleware.checkCampgroundOwnership, async (req, res) => {
+    try {
+        const foundCampground = await Campground.findById(req.params.id);
         res.render("campgrounds/edit", {campground: foundCampground});
-    });
+    } catch (err) {
+        console.error(err);
+        req.flash("error", "Error fetching campground for editing");
+        res.redirect("/campgrounds");
+    }
 });
 
 // UPDATE CAMPGROUND COUTE
-router.put("/:id", middleware.checkCampgroundOwnership , function(req, res){
-    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
-        if (err){
-            res.redirect("/campgrounds");
-        } else {
-            req.flash("success", "Campground info is updated");
-            res.redirect("/campgrounds/" + req.params.id);
-        }
-    });
+router.put("/:id", middleware.checkCampgroundOwnership, async (req, res) => {
+    try {
+        const updatedCampground = await Campground.findByIdAndUpdate(req.params.id, req.body.campground, {new: true});
+        req.flash("success", "Campground info is updated");
+        res.redirect(`/campgrounds/${updatedCampground._id}`);
+    } catch (err) {
+        console.error(err);
+        req.flash("error", "Error updating campground");
+        res.redirect("/campgrounds");
+    }
 });
 
-// UPDATE CAMPGROUND ROUTE
-// router.put("/:id", middleware.checkCampgroundOwnership, function (req, res) {
-//     // find and update the correct campground
-//     Campground.findById(req.params.id, function (err, campground) {
-//         if (err) {
-//             console.log(err);
-//             res.redirect("/campgrounds");
-//         } else {
-//             campground.name = req.body.campground.name;
-//             campground.description = req.body.campground.description;
-//             campground.image = req.body.campground.image;
-//             campground.save(function (err) {
-//                 if (err) {
-//                     console.log(err);
-//                     res.redirect("/campgrounds");
-//                 } else {
-//                     res.redirect("/campgrounds/" + campground._id);
-//                 }
-//             });
-//         }
-//     });
-// });
-
 // DESTROY CAMPGROUND ROUTE
-router.delete("/:id", middleware.checkCampgroundOwnership , function(req, res){
-    Campground.findByIdAndRemove(req.params.id, function(err){
-        if (err){
-            res.redirect("/campgrounds");
-        } else {
-            req.flash("success", "Campground deleted");
-            res.redirect("/campgrounds");
-        }
-    });
+router.delete("/:id", middleware.checkCampgroundOwnership, async (req, res) => {
+    try {
+        await Campground.findByIdAndDelete(req.params.id);
+        req.flash("success", "Campground deleted");
+        res.redirect("/campgrounds");
+    } catch (err) {
+        console.error(err);
+        req.flash("error", "Error deleting campground");
+        res.redirect("/campgrounds");
+    }
 });
 
 // Campground Like Route
-router.post("/:id/like", middleware.isLoggedIn, function (req, res) {
-    Campground.findById(req.params.id, function (err, foundCampground) {
-        if (err) {
-            console.log(err);
-            return res.redirect("/campgrounds");
-        }
-
-        // check if req.user._id exists in foundCampground.likes
-        var foundUserLike = foundCampground.likes.some(function (like) {
-            return like.equals(req.user._id);
-        });
+router.post("/:id/like", middleware.isLoggedIn, async (req, res) => {
+    try {
+        const foundCampground = await Campground.findById(req.params.id);
+        const foundUserLike = foundCampground.likes.some(like => like.equals(req.user._id));
 
         if (foundUserLike) {
-            // user already liked, removing like
             foundCampground.likes.pull(req.user._id);
         } else {
-            // adding the new user like
             foundCampground.likes.push(req.user);
         }
 
-        foundCampground.save(function (err) {
-            if (err) {
-                console.log(err);
-                return res.redirect("/campgrounds");
-            }
-            return res.redirect("/campgrounds/" + foundCampground._id);
-        });
-    });
+        await foundCampground.save();
+        res.redirect(`/campgrounds/${foundCampground._id}`);
+    } catch (err) {
+        console.error(err);
+        req.flash("error", "Error processing like");
+        res.redirect("/campgrounds");
+    }
 });
 
 // part of Fuzzy Search
